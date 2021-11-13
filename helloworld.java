8fa52ad86d7d8d701b0eea534e183e1d892023bc
IrapBrar <66657414+IrapBrar@users.noreply.github.com>
2021-11-13 11:18:23 -0800

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+//Used Java to display Hello world pattern using strings
+public class helloworld
+{
+   public static void main(String args[])
+   {
+       System.out.print("H     H   E E E E   L         L         O O O O       W       W   O O O O   R R R R   L        D D D D");
+       System.out.println("H     H   E         L         L         O     O       W       W   O     O   R     R   L        D     D");
+       System.out.println("H H H H   E E E E   L         L         O     O       W   W   W   O     O   R R R     L        D     D");
+       System.out.println("H     H   E         L         L         O     O       W   W   W   O     O   R     R   L        D     D");
+       System.out.println("H     H   E E E E   L L L L   L L L L   O O O O       W W   W W   O O O O   R     R   L L L L  D D D D");
+       }
+    }
